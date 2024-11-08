88b6b67a4635a96f3b81762c0dc102815e67a91e
Dylan Daccarett (RIT Student) <dcd7867@rit.edu>
2024-11-07 19:47:58 -0500

    hi

---
+++ server/models/Cat.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+let CatModel = {};
+
+const CatSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+
+  bedsOwned: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+
+});
+
+CatModel = mongoose.model('Cat', CatSchema);
+
+// We only want to export the cat model, so we can overwrite the entire exports object.
+module.exports = CatModel;
